2670bcf775cf89b86093e7e2de1b2589d8fa7cca
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-09-14 13:18:08 +0530

    refactore code

--- block-BRaabm/code/movie-list/src/components/MovieList.js
+++ block-BRaabm/code/movie-list/src/components/MovieList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import movies from '../data';
-import Movie from './Movie';
+import Modal from './Modal';
 class MovieList extends React.Component {
   constructor() {
     super();
@@ -8,38 +8,67 @@ class MovieList extends React.Component {
       activeIndex: null,
     };
   }
-  handleClick(index) {
-    return <Movie activeIndex={index} key={index} />;
+  handleClick = (index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+  handleToggle = () => {
+    this.setState({
+      activeIndex: null,
+    });
+  };
+  isActive() {
+    if (this.state.activeIndex || this.state.activeIndex === 0) return true;
+    else return false;
   }
   render() {
     return (
-      <div >
-        <ul className="flex justify-between px-20 flex-wrap hide">
-          {movies.map((movie, index) => (
-            <li className="flex-31 shadow-2xl rounded mb-12" key={index}>
-              <img
-                className="w-full h-xl object-cover rounded-t"
-                src={movie.Images[0]}
-                alt=""
-              />
-              <div>
-                <h3>{movie.Title}</h3>
-                <h4>Released at {movie.Released}</h4>
-                <button
-                  className="more-btn"
-                  onClick={() => {
-                    this.handleClick(index);
-                  }}
-                >
-                  More Info
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="bg-black h-screen">
-4           <Movie />
-        </div>
+      <div>
+        {!this.isActive() ? (
+          <ul
+            className="flex justify-between px-20 flex-wrap"
+          >
+            {movies.map((movie, index) => (
+              <li
+                className="flex-31 shadow-2xl rounded mb-12 relative pb-8"
+                key={index}
+              >
+                <img
+                  className="w-full h-xl object-cover rounded-t"
+                  src={movie.Images[0]}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-white text-center text-2xl mt-2">
+                    {movie.Title}
+                  </h3>
+                  <h4 className="text-white text-center">
+                    Released at {movie.Released}
+                  </h4>
+                  <button
+                    className="more-btn text-white absolute top-4 right-4"
+                    onClick={() => {
+                      this.handleClick(index);
+                    }}
+                  >
+                    More Info
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            <button
+              onClick={this.handleToggle}
+              className="absolute top-40 right-96 text-2xl"
+            >
+              ❌
+            </button>
+            <Modal activeIndex={this.state.activeIndex} />
+          </div>
+        )}
       </div>
     );
   }
