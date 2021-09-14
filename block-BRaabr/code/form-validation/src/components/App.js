8af3c362135f21b27b083e742a2e0db295260294
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-09-14 20:07:00 +0530

    Assignment: Form Validation

---
+++ block-BRaabr/code/form-validation/src/components/App.js
@@ -0,0 +1,12 @@
+import Form from './Form';
+import Registration from './Registration';
+function App() {
+  return (
+    <div>
+      {/* <Form /> */}
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
